c66ff65211bbb4ff9ef175e229c625e126286c00
CanAtilgan <76228468+CanAtilgan@users.noreply.github.com>
2021-01-20 15:23:46 +0300

    Add files via upload

---
+++ CustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Ekleme başarılı!"+ " " + customer.FirstName + " " + customer.LastName);
+            Console.WriteLine(" ");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Silindi!" + " " + customer.FirstName + " " + customer.LastName);
+            Console.WriteLine(" ");
+        }
+        public void List(Customer customer)
+        {
+            Console.WriteLine("Id: " + customer.Id);
+            Console.WriteLine("Adi: " + customer.FirstName);
+            Console.WriteLine("SoyAdi:" + customer.LastName);
+            Console.WriteLine("Kredi Puani:" + customer.CreditScore);
+            Console.WriteLine(" ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id =458315;
+            customer1.FirstName = "Can";
+            customer1.LastName = "Atilgan";
+            customer1.PhoneNumber = 05545555555;
+            customer1.IdNumber = 12345678910;
+            customer1.CreditScore = 1567;
+
+            Customer customer2 = new Customer();
+            customer2.Id =459354;
+            customer2.FirstName = "Derya";
+            customer2.LastName = "Olsun";
+            customer2.PhoneNumber= 05545555555;
+            customer2.IdNumber = 12345548910;
+            customer2.CreditScore = 1587;
+
+            Customer[] customers = new Customer[] {customer1,customer2 };
+
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.Id);
+                Console.WriteLine(customer.FirstName);
+                Console.WriteLine(customer.LastName);
+                Console.WriteLine(customer.PhoneNumber);
+                Console.WriteLine(customer.IdNumber);
+                Console.WriteLine(customer.CreditScore);
+                Console.WriteLine("*************");
+            }
+
+            Console.WriteLine("------------*****----------");
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+
+            customerManager.Delete(customer2);
+
+            customerManager.List(customer1);
+            customerManager.List(customer2);
+        }
+    }
+}
